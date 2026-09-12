c8fda0a6eaba59c11913402e798ab0e94b3d2f2c
Shourya Rastogi <rastogi.shourya123@gmail.com>
2026-09-12 13:25:06 +0530

    Commit

---
+++ src/StackAndQueues/MonotonicStackAndQueue/LargestRectangleInHistogram.java
@@ -0,0 +1,26 @@
+package StackAndQueues.MonotonicStackAndQueue;
+
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+    int largestRectangleArea(int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        int maxArea = 0;
+        for(int i=0;i<heights.length;i++){
+            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
+                int element=st.pop();
+                int nse=i;
+                int pse=(st.isEmpty())? -1:st.peek();
+                maxArea=Math.max(maxArea,heights[element]*(nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            int nse= heights.length;
+            int element=st.pop();
+            int pse=(st.isEmpty())? -1:st.peek();
+            maxArea=Math.max(maxArea,heights[element]*(nse-pse-1));
+        }
+        return maxArea;
+    }
+}
