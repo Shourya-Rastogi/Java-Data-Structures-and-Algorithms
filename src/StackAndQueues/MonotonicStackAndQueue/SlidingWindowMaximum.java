47c1cf183004e92cdef4a746b2841b6aa5623969
Shourya Rastogi <rastogi.shourya123@gmail.com>
2026-09-13 17:47:16 +0530

    Commit

---
+++ src/StackAndQueues/MonotonicStackAndQueue/SlidingWindowMaximum.java
@@ -0,0 +1,8 @@
+package StackAndQueues.MonotonicStackAndQueue;
+import java.util.*;
+public class SlidingWindowMaximum {
+    int[] findmax(int[] arr, int k){
+        List<Integer> list=new ArrayList<>();
+
+    }
+}
