f9ed0aec1d95b07886cb8952540a95fdb43e4d4c
Shourya Rastogi <rastogi.shourya123@gmail.com>
2026-09-13 22:55:01 +0530

    Commit

---
+++ src/StackAndQueues/MonotonicStackAndQueue/StockSpanProblem.java
@@ -0,0 +1,20 @@
+package StackAndQueues.MonotonicStackAndQueue;
+import java.util.Stack;
+
+
+public class StockSpanProblem {
+    private Stack<int[]> stack;
+
+    public StockSpanProblem() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1]; // Add popped span
+        }
+        stack.push(new int[]{price, span}); // Store price and span
+        return span;
+    }
+}
