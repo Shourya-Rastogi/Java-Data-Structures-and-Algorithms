059d8ebd2e0ce8a075e50373163bb13aac8d5932
Shourya Rastogi <rastogi.shourya123@gmail.com>
2026-09-23 01:57:20 +0530

    Commit

---
+++ src/BinaryTrees/LevelOrderTraversal.java
@@ -0,0 +1,29 @@
+package BinaryTrees;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    List<List<Integer>> levelorder(TreeNode root){
+        Queue<TreeNode> queue= new LinkedList<>();
+        List<List<Integer>> wrapList=new LinkedList<List<Integer>>();
+        if(root==null) return wrapList;
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum=queue.size();
+            List<Integer> subList=new LinkedList<Integer>();
+            for(int i=0;i<levelNum;i++){
+                if(queue.peek().left!=null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right!=null){
+                    queue.offer(queue.peek().right);
+                    subList.add(queue.poll().data);
+                }
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+}
