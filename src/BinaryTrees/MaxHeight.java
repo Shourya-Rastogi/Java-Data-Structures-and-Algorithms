ff6dd77e60632968a036323111235a722a1805ea
Shourya Rastogi <rastogi.shourya123@gmail.com>
2026-09-26 00:07:05 +0530

    Commit

---
+++ src/BinaryTrees/MaxHeight.java
@@ -0,0 +1,10 @@
+package BinaryTrees;
+
+public class MaxHeight {
+    int maxDepth(TreeNode root){
+        if(root==null) return 0;
+        int lh=maxDepth(root.left);
+        int rh=maxDepth(root.right);
+        return 1+Math.max(lh,rh);
+    }
+}
